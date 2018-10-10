6b9f484f08eb2d93a1e3b6fd118d0bc5de45974f
gonpeche <espeche.g@gmail.com>
2018-10-10 18:34:16 -0300

    update

--- browser/react/components/NewPlayList.jsx
+++ browser/react/components/NewPlayList.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 
-const NewPlayList = () => (
-    <div className="well">
-      <form className="form-horizontal">
-        <fieldset>
-          <legend>New Playlist</legend>
-          <div className="form-group">
-            <label className="col-xs-2 control-label">Name</label>
-            <div className="col-xs-10">
-              <input className="form-control" type="text"/>
+class NewPlayList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: ''
+        }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit (evt) {
+        evt.preventDefault()
+        console.log(evt)
+        const value = evt.target.value;
+        this.setState({
+           inputValue: value
+        });
+        console.log(evt.target.value)
+    }
+
+    render() {
+        return (
+            <div className="well">
+              <form className="form-horizontal" value='Submit' onSubmit={this.handleSubmit}>
+                <fieldset>
+                  <legend>New Playlist</legend>
+                  <div className="form-group">
+                    <label className="col-xs-2 control-label">Name</label>
+                    <div className="col-xs-10">
+                      <input className="form-control" type="text"/>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-xs-10 col-xs-offset-2">
+                      <button type="submit" className="btn btn-success">Create Playlist</button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+        
+        
             </div>
-          </div>
-          <div className="form-group">
-            <div className="col-xs-10 col-xs-offset-2">
-              <button type="submit" className="btn btn-success">Create Playlist</button>
-            </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-);
+        )
+
+    }
+
+}
+
 
 export default NewPlayList;
